08946279e4c5c8d6dcccf4800c54fd42736b40bf
Torben Lochow <t.lochow@gmx.de>
2022-03-05 13:22:03 +0100

    Improved Navigation Builder and Data Loading.

--- js/navigationBuilder.js
+++ js/navigationBuilder.js
@@ -1,7 +1,17 @@
-$.getJSON("https://spritland.github.io/tracker/data.json", function (json) {
-    json.Players.sort(compareByName);
-    json.Players.forEach(function (player) {
-        $('#PlayersNavigation').append('<a class="nav-link" href="players.html?playerId=' + player.Id + '">' + player.Name + '</a>')
+$(function () {
+    $("#layoutSidenav_nav").load("navigation.html");
+
+    $.getJSON("data.json", function (json) {
+        // Set new default font family and font color to mimic Bootstrap's default styling
+        Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+        Chart.defaults.global.defaultFontColor = '#292b2c';
+
+        BindSiteData(json);
+
+        json.Players.sort(compareByName);
+        json.Players.forEach(function (player) {
+            $('#PlayersNavigation').append('<a class="nav-link" href="players.html?playerId=' + player.Id + '">' + player.Name + '</a>')
+        });
     });
 });
 
